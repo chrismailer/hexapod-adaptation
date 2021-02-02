db2d47036c45b31389775cdd89dcbe4bf2d348a5
Chris Mailer <christophermailer@icloud.com>
2021-02-02 15:29:05 +0200

    Renamed

--- Code/maps/map_plot.py
+++ Code/maps/map_plot.py
@@ -3,7 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-def plot_map(data, map_num):
+def plot_map(data, map_num, niches):
 	# matplotlib.use("pgf")
 	matplotlib.rcParams.update({
 	    "pgf.texsystem": "pdflatex",
@@ -15,15 +15,15 @@ def plot_map(data, map_num):
 	fig, ax = plt.subplots()
 	fig.set_size_inches(w=4.7747, h=3.5)
 
-	A = np.zeros((125, 125))
+	dim = int(niches ** (1/6) - 1)
+
+	A = np.zeros((dim**3, dim**3))
 
 	max_index = np.argmax(data[:,0])
 
 	for index, row in enumerate(data):
 		fitness = row[0] / 5
 		desc = row[1:7]
-
-		dim = 5
 
 		x = int((np.ceil(desc[0]*dim)-1)*(dim**2) + (np.ceil(desc[1]*dim)-1)*dim + (np.ceil(desc[2]*dim)-1))
 		y = int((np.ceil(desc[5]*dim)-1)*(dim**2) + (np.ceil(desc[4]*dim)-1)*dim + (np.ceil(desc[3]*dim)-1))
@@ -37,7 +37,7 @@ def plot_map(data, map_num):
 	plt.scatter((y/124)*100, (x/124)*100, color='red', s=4, marker='s')
 
 	# plt.imshow(A, interpolation='none', origin='lower', vmin=0, vmax=0.5, extent=(0, 100, 0, 100))
-	plt.imshow(A, interpolation='none', origin='lower', extent=(0, 100, 0, 100), cmap='inferno', vmin=0, vmax=0.6)
+	plt.imshow(A, interpolation='gaussian', origin='lower', extent=(0, 100, 0, 100), cmap='inferno', vmin=0, vmax=0.6)
 	clb = plt.colorbar()
 	clb.ax.set_ylabel('Fitness ($m/s$)')
 
@@ -62,7 +62,8 @@ def plot_map(data, map_num):
 
 
 if __name__ == "__main__":
-	map_num = 1
-	data = np.loadtxt('../maps/niches_20000/map_%d.dat' % map_num)
+	map_num = 8
+	niches = 20000
+	data = np.loadtxt(f'./niches_{niches}/map_{map_num}.dat')
 	# data = np.loadtxt('../real_experiments/adapted_map_3.dat')
-	plot_map(data, map_num)
+	plot_map(data, map_num, niches)
