b86f4440d6314afabf4b92b2901595a91d95e501
Chris Mailer <christophermailer@icloud.com>
2021-03-30 15:38:00 +0200

    Removed hexapod simulator

--- Scripts/hexapod/example.py
+++
@@ -1,8 +0,0 @@
-from simulator import Simulator
-from controllers.stabilised import Controller, tripod_gait
-
-controller = Controller(tripod_gait, body_height=0.12, velocity=0.4, crab_angle=-1.57)
-simulator = Simulator(controller, urdf='/urdf/hexapod_simplified.urdf', follow=True, visualiser=True, collision_fatal=False, failed_legs=[])
-
-while True:
-	simulator.step()
